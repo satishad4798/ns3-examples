fd618e3edc5cfdc96be5de9e7ad0b235daf9f22a
EvanKerr <55823154+EvanKerr@users.noreply.github.com>
2019-09-26 09:34:18 +0530

    create dumbell.cc

---
+++ dumbell.cc
@@ -0,0 +1,103 @@
+//half dumbell
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("SimpleGlobalRoutingExample");
+
+int 
+main (int argc, char *argv[])
+{
+  // Users may find it convenient to turn on explicit debugging
+  // for selected modules; the below lines suggest how to do this
+#if 0 
+  LogComponentEnable ("SimpleGlobalRoutingExample", LOG_LEVEL_INFO);
+#endif
+
+  // Set up some default values for the simulation.  Use the 
+  Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (210));
+  Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("448kb/s"));
+
+  //DefaultValue::Bind ("DropTailQueue::m_maxPackets", 30);
+
+  // Allow the user to override any of the defaults and the above
+  // DefaultValue::Bind ()s at run-time, via command-line arguments
+  CommandLine cmd;
+  bool enableFlowMonitor = false;
+  cmd.AddValue ("EnableMonitor", "Enable Flow Monitor", enableFlowMonitor);
+  cmd.Parse (argc, argv);
+
+  // Here, we will explicitly create four nodes.  In more sophisticated
+  // topologies, we could configure a node factory.
+  NS_LOG_INFO ("Create nodes.");
+  NodeContainer c;
+  c.Create (3);
+  NodeContainer n0n2 = NodeContainer (c.Get (0), c.Get (1));
+  NodeContainer n2n3 = NodeContainer (c.Get (1), c.Get (2));
+
+  InternetStackHelper internet;
+  internet.Install (c);
+
+  // We create the channels first without any IP addressing information
+  NS_LOG_INFO ("Create channels.");
+  PointToPointHelper p2p;
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  NetDeviceContainer d0d2 = p2p.Install (n0n2);
+
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  NetDeviceContainer d2d3 = p2p.Install (n2n3);
+
+  // Later, we add IP addresses.
+  NS_LOG_INFO ("Assign IP Addresses.");
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer i0i2 = ipv4.Assign (d0d2);
+
+  ipv4.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer i2i3 = ipv4.Assign (d2d3);
+
+  // Create router nodes, initialize routing database and set up the routing
+  // tables in the nodes.
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  uint16_t port = 9;   // Discard port (RFC 863)
+  BulkSendHelper source ("ns3::TcpSocketFactory",
+                         InetSocketAddress (i0i2.GetAddress (1), port));
+  // Set the amount of data to send in bytes.  Zero is unlimited.
+  source.SetAttribute ("MaxBytes", UintegerValue (0));
+  ApplicationContainer apps = source.Install (n0n2.Get (0));
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (10.0));
+
+  
+
+  // Create a packet sink to receive these packets
+  PacketSinkHelper sink ("ns3::TcpSocketFactory",
+                         Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+  ApplicationContainer sinkApps = sink.Install (c.Get (2));
+  sinkApps.Start (Seconds (1.0));
+  sinkApps.Stop (Seconds (10.0));
+
+  NS_LOG_INFO ("Run Simulation.");
+  Simulator::Stop (Seconds (11));
+  Simulator::Run ();
+  NS_LOG_INFO ("Done.");
+  Simulator::Destroy ();
+
+  Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkApps.Get (0));
+  std::cout << "Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
+  return 0;
+}
